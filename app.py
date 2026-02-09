2b419ab47a3792a33da59cbb972576f65abe0948
Gabriel-Anselmo-Pelogia <ganselmopelogia@gmail.com>
2026-02-09 09:55:28 -0300

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    nome = None
+
+    if request.method == "POST":
+        nome = request.form.get("nome")
+
+    return render_template("index.html", nome=nome)
+
+@app.route("/dashboard")
+def dashboard():
+    dados = [10, 20, 30, 40]
+    labels = ["Jan", "Fev", "Mar", "Abr"]
+
+    return render_template(
+        "dashboard.html",
+        dados=dados,
+        labels=labels
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
