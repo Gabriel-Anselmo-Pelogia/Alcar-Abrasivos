7b62dcc15fa94654e9e0685a563ddea21406bb7a
Gabriel-Anselmo-Pelogia <ganselmopelogia@gmail.com>
2026-02-10 12:02:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 st.set_page_config(layout="wide")
 
-# Estado inicial
+# Estado
 if "menu" not in st.session_state:
     st.session_state.menu = "analise"
 
@@ -11,21 +11,29 @@ if "menu" not in st.session_state:
 css = Path("frontend/sidebar.css").read_text(encoding="utf-8")
 st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
 
-# HTML
-html = Path("frontend/sidebar.html").read_text(encoding="utf-8")
-
-html = html.replace(
-    "{{analise}}",
-    "active" if st.session_state.menu == "analise" else ""
-)
-
-html = html.replace(
-    "{{estoque}}",
-    "active" if st.session_state.menu == "estoque" else ""
-)
-
-# Sidebar (APENAS UM MENU)
 with st.sidebar:
+
+    col1 = st.container()
+    if col1.button("Análise de Dados", key="btn_analise"):
+        st.session_state.menu = "analise"
+
+    col2 = st.container()
+    if col2.button("Gerenciamento de Estoque", key="btn_estoque"):
+        st.session_state.menu = "estoque"
+
+    # HTML visual
+    html = Path("frontend/sidebar.html").read_text(encoding="utf-8")
+
+    html = html.replace(
+        "{{analise}}",
+        "active" if st.session_state.menu == "analise" else ""
+    )
+
+    html = html.replace(
+        "{{estoque}}",
+        "active" if st.session_state.menu == "estoque" else ""
+    )
+
     st.markdown(html, unsafe_allow_html=True)
 
 # Conteúdo
